9ea5dd1a4f9af21aa24bfff684601f6ab3bcc95c
sahilkhan09k <sahilkhan392005@gmail.com>
2025-01-02 12:06:16 +0530

    utility file added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,25 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path : './env'
 })
 
-connectDB();
+connectDB().then(() => {
+    let port = process.env.PORT || 4000;
+
+    app.on("error", (err) => {
+        console.log("error !!", err)
+    })
+    app.listen(port, () => {
+        console.log(`server is running on the port ${port}`)
+    })
+})
+.catch((error) => {
+    console.log(`MONGODB CONNECTION ERROR FAILED`, error);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (fn) => {
+        (req, res, next) => {
+            Promise.resolve(fn(req, res, next)).catch((error) => next(error))
+        }
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler2 = (func) => {async () => {}}
+// const asyncHandler2 = (func) => async (req, res, next) => {
+//     try {
+//        await func(req, res, next) 
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message : err.message,
+//         })
+//     }
+// }
