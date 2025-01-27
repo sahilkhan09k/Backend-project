9ef69b4b1211e89286eaea3af292cff3fe21d2ca
sahilkhan09k <sahilkhan392005@gmail.com>
2025-01-27 22:07:44 +0530

    all routers added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser,changeProfileDetails,updateUserAvatar,updateCoverImage} from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser,changeProfileDetails,updateUserAvatar,updateCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import {verifyJwt} from "../middlewares/auth.middleware.js";
 
@@ -25,16 +25,21 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJwt,logoutUser)
+
 router.route("/Refresh-token").post(refreshAccessToken)
+
 router.route("/change-password").post(verifyJwt, changeCurrentPassword)
-router.route("/current-user").post(verifyJwt, getCurrentUser)
-router.route("/Change-details").post(verifyJwt, changeProfileDetails)
-router.route("/update-avatar").post(upload.fields([{
-   name : "avatar",
-   maxCount : 1
-}]), verifyJwt, updateUserAvatar)
-router.route("/update-coverImage").post(upload.fields([{
-   name : "coverimage",
-   maxCount : 1
-}]), verifyJwt, updateCoverImage)
+
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+
+router.route("/Change-details").patch(verifyJwt, changeProfileDetails)
+
+router.route("/update-avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+
+
+router.route("/update-coverImage").patch(verifyJwt, upload.single("coverimage"), updateCoverImage)
+
+router.route("/c/:userName").get(verifyJwt, getUserChannelProfile)
+
+router.route("/history").get(verifyJwt, getWatchHistory)
 export default router;
